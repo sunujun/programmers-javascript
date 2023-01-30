2ac90c2b831d74517c727a420a4d96e9f84cb38d
seongjun.oh <tiger8708@gmail.com>
2023-01-30 19:47:27 +0900

    두 수의 곱

---
+++ 두 수의 곱.js
@@ -0,0 +1,17 @@
+/**
+ * @ts-check
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120804
+ */
+
+/**
+ * 두 수의 곱을 출력하는 함수
+ *
+ * @param {number} num1 첫 번째 정수
+ * @param {number} num2 두 번째 정수
+ * @returns {number} 주어진 두 정수의 곱
+ */
+function solution(num1, num2) {
+  let answer = num1 * num2;
+
+  return answer;
+}
