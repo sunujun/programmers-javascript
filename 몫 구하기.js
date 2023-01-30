97e561495b218b3887d03c843cc8404cb3553d4a
seongjun.oh <tiger8708@gmail.com>
2023-01-30 19:51:48 +0900

    몫 구하기

---
+++ 몫 구하기.js
@@ -0,0 +1,17 @@
+/**
+ * @ts-check
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120805
+ */
+
+/**
+ * 첫 번째 정수를 두 번째 정수로 나눈 몫을 출력하는 함수
+ *
+ * @param {number} num1 첫 번째 정수
+ * @param {number} num2 두 번째 정수
+ * @returns {number} 첫 번째 정수를 두 번째 정수로 나눈 몫
+ */
+function solution(num1, num2) {
+  let answer = parseInt(num1 / num2);
+
+  return answer;
+}
