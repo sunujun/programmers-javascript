46a1b68ec3e15122900a4d3f25e3f5c03d09ef2c
seongjun.oh <tiger8708@gmail.com>
2023-01-30 19:43:33 +0900

    두 수의 합

---
+++ 두 수의 합.js
@@ -0,0 +1,17 @@
+/**
+ * @ts-check
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120802
+ */
+
+/**
+ * 두 수의 합을 출력하는 함수
+ *
+ * @param {number} num1 첫번째 정수
+ * @param {number} num2 두번째 정수
+ * @returns {number} 주어진 두 정수의 합
+ */
+function solution(num1, num2) {
+  let answer = num1 + num2;
+
+  return answer;
+}
