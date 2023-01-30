2b9d0ef49a5a4f3b96aa07b7ec52d38f8c3a1b2e
seongjun.oh <tiger8708@gmail.com>
2023-01-30 19:45:36 +0900

    두 수의 합

--- 두 수의 합.js
+++ 두 수의 합.js
@@ -6,8 +6,8 @@
 /**
  * 두 수의 합을 출력하는 함수
  *
- * @param {number} num1 첫번째 정수
- * @param {number} num2 두번째 정수
+ * @param {number} num1 첫 번째 정수
+ * @param {number} num2 두 번째 정수
  * @returns {number} 주어진 두 정수의 합
  */
 function solution(num1, num2) {
