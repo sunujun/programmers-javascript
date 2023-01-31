5c609cfc346c27b4688a2aa16b5f6c74be0041e7
seongjun.oh <tiger8708@gmail.com>
2023-01-31 19:42:04 +0900

    나머지 구하기

---
+++ 나머지 구하기.js
@@ -0,0 +1,17 @@
+/**
+ * @ts-check
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120810
+ */
+
+/**
+ * 첫 번째 정수를 두 번째 정수로 나눈 나머지를 출력하는 함수
+ *
+ * @param {number} num1 첫 번째 정수
+ * @param {number} num2 두 번째 정수
+ * @returns {number} 첫 번째 정수를 두 번째 정수로 나눈 나머지
+ */
+function solution(num1, num2) {
+  const answer = num1 % num2;
+
+  return answer;
+}
